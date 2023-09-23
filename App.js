f5741bec008f0849803104e7d2ce35aff7861de6
Musharraf Dhalwala <dhalwalamusharraf1210@gmail.com>
2023-09-23 14:09:47 +0530

    Message

--- App.js
+++ App.js
@@ -11,10 +11,33 @@ const heading=React.createElement(
 //JSX - HTML Like or XML Like Syntax
 
 //JSX ==>Babel Transpiled it to React.createElement  ==>React Element is JS Object ==> when renderd it will be converted to "HTML ELEMENT"...
-const jsxHeading=<h1 className="heading">Namaste React Using JSX</h1>
+const jsxHeading=(
+    <h1 className="heading">Namaste React Using JSX</h1>
+)
 
-console.log(jsxHeading);
+//react element
+const elem=<span>React Element</span>
+
+
+//React Components
+const Title= ()=>(
+    <h1 className="Title">
+        Namaste React From Title
+    </h1>
+)
+
+const HeadingComponent =()=>
+{
+    return (
+     <div id="container">
+        {elem}
+        <Title/>
+        <h1>Namaste React From Functional Components</h1>
+    </div>
+    )
+}
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+
+root.render(<HeadingComponent/>);
