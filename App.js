25912e5e4c5369999bf5c5080e9e012309977ea1
Musharraf Dhalwala <dhalwalamusharraf1210@gmail.com>
2023-09-23 11:54:24 +0530

    Message

--- App.js
+++ App.js
@@ -1,2 +1,20 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom/client";
+
+//React.createElement  ==>will create React Element which is JS Object ==> when renderd it will be converted to "HTML ELEMENT"
+const heading=React.createElement(
+    "h1",
+    {id:"heading"},
+    "Namaste React"
+);
+
+//JSX - HTML Like or XML Like Syntax
+
+//JSX ==>Babel Transpiled it to React.createElement  ==>React Element is JS Object ==> when renderd it will be converted to "HTML ELEMENT"...
+const jsxHeading=<h1 className="heading">Namaste React Using JSX</h1>
+
+console.log(jsxHeading);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
